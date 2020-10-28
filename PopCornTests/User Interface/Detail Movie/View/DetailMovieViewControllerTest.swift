9cad8c38d781ac09b3bafb8c98b70d1ad1aaedd6
valmirt <valmirt.dev@gmail.com>
2020-10-27 21:54:08 -0300

    detail movie unit test

--- PopCornTests/User Interface/Detail Movie/View/DetailMovieViewControllerTest.swift
+++ PopCornTests/User Interface/Detail Movie/View/DetailMovieViewControllerTest.swift
@@ -11,18 +11,59 @@ import XCTest
 
 class DetailMovieViewControllerTest: XCTestCase {
     
+    var fakeMovieRepository: FakeMovieRepository?
+    
     //System Under Test
     var sut: DetailMovieViewController?
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        fakeMovieRepository = FakeMovieRepository()
+        sut = DetailMovieViewController.instatiate(from: .detailMovie)
+        sut?.viewModel = DetailMovieViewModel(idMovie: FakeMovieNetworkManager.ID, fakeMovieRepository!)
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        fakeMovieRepository = nil
+        sut = nil
         super.tearDown()
     }
     
+    func testLoadingHidden() {
+        //Given
+        let promise = expectation(description: "Success!")
+        
+        //When
+        let _ = sut?.view
+        
+        //Then
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            promise.fulfill()
+            XCTAssertTrue(self.sut?.loadingSpinner.isHidden ?? false)
+        }
+        wait(for: [promise], timeout: 2.0)
+    }
     
+    func testCorrectDataShow() {
+        //Given
+        let promise = expectation(description: "Success!")
+        let expected = FakeMovieNetworkManager.fakeDetail()
+        
+        //When
+        let _ = sut?.view
+        
+        //Then
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            promise.fulfill()
+            XCTAssertEqual(self.sut?.labelTitle.text!, expected.title)
+            XCTAssertEqual(self.sut?.labelGenres.text!, expected.genresFormatted)
+            XCTAssertEqual(self.sut?.labelCountries.text!, expected.countriesFormatted)
+            XCTAssertEqual(self.sut?.labelStatus.text!, expected.status)
+            XCTAssertEqual(self.sut?.labelReleaseDate.text!, expected.yearDate)
+            XCTAssertEqual(self.sut?.labelRuntime.text!, "\(expected.runtime!) min")
+            XCTAssertEqual(self.sut?.tvOverview.text!, expected.overview)
+            XCTAssertEqual(self.sut?.labelCompanies.text!, expected.companiesFormatted)
+        }
+        wait(for: [promise], timeout: 2.0)
+    }
 }
